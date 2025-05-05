61580e100278ec98288e70c66d5e983683d6cc88
shashwat sharma <shashwatsharmapalak@gmail.com>
2025-05-05 10:45:17 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"; 
 import Home from "./Home";
-import SearchPage from "./SearchPage"; // Ensure you have SearchPage defined
+import SearchPage from "./SearchPage";
 
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/home" />} /> {/* Redirect to /home */}
+        <Route path="/" element={<Navigate to="/home" />} /> 
         <Route path="/home" element={<Home />} />
-        <Route path="/search" element={<SearchPage />} /> {/* Add the SearchPage route */}
+        <Route path="/search" element={<SearchPage />} /> 
       
       </Routes>
     </Router>
